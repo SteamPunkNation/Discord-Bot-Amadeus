62aea360bd332bc9153c0dfcca993d0f1b207328
SteamPunkNation <steampunknation.ad@gmail.com>
2023-03-31 00:42:16 -0400

    Added new file: LICENSE
    Modified the following:
    .gitignore
    Readme.md
    index.js
    package-lock.json
    package.json
    temp-config.json

--- Readme.md
+++ Readme.md
@@ -1,16 +1,42 @@
 # Amadeus Discord Bot
 
-In development.
-To start bot type
+Amadeus is a Discord bot that can provide users with various commands, including gifme, help, quoteoftheday, and whoareyou. It also uses OpenAI's GPT to respond to users with specific roles in specific servers listed in the config.json file.
 
-> npm run start
+## Usage
 
-To test for errors type
+To use Amadeus you can use the following commands:
 
-> npm run lint
+- /gifme: Returns a random GIF based on the anime Steins;Gate and Steins;Gate 0.
+- /help: Displays a list of available commands and their descriptions.
+- /quoteoftheday: Returns a random quote of the day.
+- /whoareyou: Returns a description of what Amadeus is and what it can do.
+  > To interact with the bot using OpenAI's GPT, you must have the correct role assigned to your user in the specific server listed in the config.json file, and mention the bot in the chat for it to reply back.
 
-You can edit lint in the package.json to add eslint . --fix to automatically fix errors in the code.
+## Installation
 
-## Setup
+If you wish to run Amadeus locally, follow these steps:
 
-In order to starup your own version you must rename the temp-config.json file in the root directory to config.json and replace the values with your own.
+1. Clone the repository: git clone https://github.com/SteamPunkNation/Discord-Bot-Amadeus.git
+2. Install dependencies: npm install
+3. Set up your config.json file with your Discord bot token, OpenAI API key, and other configuration options.
+   > Note the original config.json file does not exist, you must rename temp-config.json to config.json and replace the values needed.
+4. Run the bot: npm run start
+   > You must leave the console running in order to keep the bot online.
+
+## Configuration
+
+The config.json file contains the following options:
+
+- token: Your Discord bot token.
+- openaiKey: Your OpenAI API key.
+- system: The prefix for system messages logged by the bot.
+- guildId: The ID of the server in which the bot will listen for chat input.
+- allowedRoles: An array of roles that are allowed to use the GPT feature.
+
+## Credits
+
+Amadeus was created by [SteamPunkNation](https://github.com/SteamPunkNation> "Github") using the [Discord.js](https://discord.js.org/ "Discord.js") library and OpenAI's [GPT-3 ](https://openai.com/ "Chat GPT") API.
+
+## License
+
+Amadeus is licensed under the [MIT License](https://github.com/SteamPunkNation/Discord-Bot-Amadeus/blob/main/LICENSE "MIT License").
--- index.js
+++ index.js
@@ -1,58 +1,100 @@
+// Import modules
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-const system = '(Amadeus System) ';
+const { Configuration, OpenAIApi } = require('openai');
+const { token, guildId, openaiKey, botPrompt, system, allowedRoles } = require('./config.json');
 
+// Set up the Discord client
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-
 client.commands = new Collection();
+
+// Load commands
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 }
 
-// Output to console that bot is online.
-client.once(Events.ClientReady, () => {
-	console.log(system + `Logged in as ${client.user.tag}`);
-	console.log(system + 'Now online!');
-});
-
-// Check if latest message is a command.
+// Handle chat input commands
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	const command = client.commands.get(interaction.commandName);
-
 	if (!command) return;
 
-	// Execute command if errors catch case.
 	try {
 		await command.execute(interaction);
 	}
 	catch (error) {
 		console.error(error);
+		const content = system + 'There was an error while executing this command!';
 		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: system + 'There was an error while executing this command!', ephemeral: true });
+			await interaction.followUp({ content, ephemeral: true });
 		}
 		else {
-			await interaction.reply({ content: system + 'There was an error while executing this command!', ephemeral: true });
+			await interaction.reply({ content, ephemeral: true });
 		}
 	}
 });
 
-// Check if message from user (NOT BOTS) contains Amadeus mention, if so reply back to user with same message.
+// Handle messages from users
 client.on(Events.MessageCreate, async message => {
 	if (message.author.bot) return;
 
-	if (message.mentions.has(client.user)) {
-		console.log(system + `Received mention from ${message.author.username}: ${message.content}`);
-		message.reply(`Hi ${message.author} I am Amadeus. You said ${message.content}`);
+	if (guildId != message.guildId) {
+		return message.reply(`Sorry ${message.author} You do not have permission to talk to me in this server.`);
+	}
+
+	const serverRoles = message.member.roles.cache.map((role) => role.name);
+	if (!serverRoles.some((role) => allowedRoles.includes(role))) {
+		return message.reply(`Sorry ${message.author} You do not have permission to talk to me.`);
+	}
+
+	const input = message.content.slice(`<@!${client.user.id}>`.length).trim();
+	if (!input) return;
+
+	console.log(system + `Received mention from ${message.author.username}: ${input}`);
+
+	const configuration = new Configuration({ apiKey: openaiKey });
+	const openai = new OpenAIApi(configuration);
+
+	try {
+		const output = await generateResponse(openai, `${botPrompt}:${input.trim()}`);
+		message.channel.send(output);
+	}
+	catch (error) {
+		console.log(error);
+		message.reply('An error occurred while processing your message. Please try again later.');
 	}
 });
 
-client.login(token);
+// Generate AI response
+async function generateResponse(openai, prompt) {
+	const response = await openai.createCompletion({
+		model: 'text-davinci-003',
+		prompt,
+		temperature: 0.7,
+		max_tokens: 100,
+		top_p: 1.0,
+		frequency_penalty: 0.5,
+		presence_penalty: 0,
+	});
+
+	const output = response.data.choices[0].text.trim();
+	return output;
+
+}
+
+// Output to console that bot is online.
+client.login(token)
+	.then(() => {
+		console.log(system + `Logged in as ${client.user.tag}`);
+		console.log(system + 'Now online!');
+	})
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
