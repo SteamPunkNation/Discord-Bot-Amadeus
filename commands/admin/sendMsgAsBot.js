8b91823dcb7152a8ee8f51600d2d3c5d41eca58b
SteamPunkNation <steampunknation.ad@gmail.com>
2024-05-01 19:19:20 -0400

    Start of server analytics command

--- commands/admin/sendMsgAsBot.js
+++ commands/admin/sendMsgAsBot.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { allowedRoles } = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,8 +15,11 @@ module.exports = {
         .setDMPermission(false),
 
     async execute(interaction) {
+        const who = interaction.user.username; 
         const msg = interaction.options.getString('msg');
-        console.log(msg);
+
+        console.log(`${who} told Amadeus to say: ${msg}`)
+        
         await interaction.deferReply();
         await interaction.deleteReply();
         await interaction.channel.send(msg);
